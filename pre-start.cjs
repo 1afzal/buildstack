3df66d28685cfd40e0103f14713d535c3bdbc410
1afzal <afzalali5256@gmail.com>
2026-04-15 15:35:14 +0530

    fixed vercel deployment error

--- pre-start.cjs
+++ pre-start.cjs
@@ -14,13 +14,5 @@ let commitJson = {
   version: JSON.stringify(process.env.npm_package_version),
 };
 
-console.log(`
-★═══════════════════════════════════════★
-          B O L T . D I Y
-         ⚡️  Welcome  ⚡️
-★═══════════════════════════════════════★
-`);
 console.log('📍 Current Version Tag:', `v${commitJson.version}`);
 console.log('📍 Current Commit Version:', commitJson.hash);
-console.log('  Please wait until the URL appears here');
-console.log('★═══════════════════════════════════════★');
